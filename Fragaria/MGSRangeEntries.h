63264e30f6e744343e0e0b9d630b8bae0368ad7e
Daniele Cattaneo <daniele.cattaneo@alice.it>
2019-04-24 21:04:39 +0200

    Cleanup of MGSRangeEntries

--- Fragaria/MGSRangeEntries.h
+++ Fragaria/MGSRangeEntries.h
@@ -1,3 +1,10 @@
+//
+//  MGSRangeEntries.h
+//  Fragaria
+//
+//  Copyright (c) 2019 Daniele Cattaneo
+//
+
 /* Copyright (c) 2006-2007 Christopher J. W. Lloyd
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -12,25 +19,25 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 typedef struct MGSRangeEntries MGSRangeEntries;
 
 typedef struct {
-   MGSRangeEntries *self;
-   NSUInteger      index;
+    MGSRangeEntries *self;
+    NSUInteger index;
 } MGSRangeEnumerator;
 
-FOUNDATION_EXPORT MGSRangeEntries *MGSCreateRangeToCopiedObjectEntries(NSUInteger capacity);
+MGSRangeEntries *MGSCreateRangeToCopiedObjectEntries(NSUInteger capacity);
 
-FOUNDATION_EXPORT void MGSFreeRangeEntries(MGSRangeEntries *self);
-FOUNDATION_EXPORT void  MGSResetRangeEntries(MGSRangeEntries *self);
-FOUNDATION_EXPORT NSUInteger MGSCountRangeEntries(MGSRangeEntries *self);
-FOUNDATION_EXPORT void MGSRangeEntriesRemoveEntryAtIndex(MGSRangeEntries *self,NSUInteger index);
+void MGSFreeRangeEntries(MGSRangeEntries *self);
+void MGSResetRangeEntries(MGSRangeEntries *self);
+NSUInteger MGSCountRangeEntries(MGSRangeEntries *self);
+void MGSRangeEntriesRemoveEntryAtIndex(MGSRangeEntries *self, NSUInteger index);
 
-FOUNDATION_EXPORT void  MGSRangeEntryInsert(MGSRangeEntries *self,NSRange range,id value);
-FOUNDATION_EXPORT id MGSRangeEntryAtIndex(MGSRangeEntries *self,NSUInteger index,NSRange *effectiveRange);
-FOUNDATION_EXPORT id MGSRangeEntryAtRange(MGSRangeEntries *self,NSRange range);
+void MGSRangeEntryInsert(MGSRangeEntries *self, NSRange range, id value);
+id MGSRangeEntryAtIndex(MGSRangeEntries *self, NSUInteger index, NSRange *effectiveRange);
+id MGSRangeEntryAtRange(MGSRangeEntries *self, NSRange range);
 
-FOUNDATION_EXPORT MGSRangeEnumerator MGSRangeEntryEnumerator(MGSRangeEntries *self);
-FOUNDATION_EXPORT BOOL MGSNextRangeEnumeratorEntry(MGSRangeEnumerator *state,NSRange *rangep, id *value);
+MGSRangeEnumerator MGSRangeEntryEnumerator(MGSRangeEntries *self);
+BOOL MGSNextRangeEnumeratorEntry(MGSRangeEnumerator *state, NSRange *rangep, id *value);
 
-FOUNDATION_EXPORT void MGSRangeEntriesExpandAndWipe(MGSRangeEntries *self,NSRange range,NSInteger delta);
-FOUNDATION_EXPORT void MGSRangeEntriesDivideAndConquer(MGSRangeEntries *self,NSRange range);
-FOUNDATION_EXPORT void MGSRangeEntriesDump(MGSRangeEntries *self);
-FOUNDATION_EXPORT void MGSRangeEntriesVerify(MGSRangeEntries *self,NSUInteger length);
+void MGSRangeEntriesExpandAndWipe(MGSRangeEntries *self, NSRange range, NSInteger delta);
+void MGSRangeEntriesDivideAndConquer(MGSRangeEntries *self, NSRange range);
+void MGSRangeEntriesDump(MGSRangeEntries *self);
+void MGSRangeEntriesVerify(MGSRangeEntries *self, NSUInteger length);
