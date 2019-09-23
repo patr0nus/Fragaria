patr0nus/Fragaria

commit 3d0dfb9346912329cde6492bcad5364a30f44b4b

Allow '^' and '$' to match the beginning and the end of each text line in the syntax definition regexes

Fragaria/MGSClassicFragariaSyntaxDefinition.m
@@ -93,7 +93,8 @@ NSString *SMLSyntaxDefinitionGroupSpecialization = @"groupSpecialization";
     value = [syntaxDictionary objectForKey:SMLSyntaxDefinitionAlternativeNumberRegex];
     if (value) {
         RETURN_NIL_IF_FALSE([value isKindOfClass:[NSString class]], @"NSString expected");
-        _numberDefinition = value;
+        _numberDefinition = [NSRegularExpression regularExpressionWithPattern:value options:NSRegularExpressionAnchorsMatchLines error:nil];
+        RETURN_NIL_IF_FALSE(_numberDefinition, @"Incorrect regex syntax in %@", SMLSyntaxDefinitionAlternativeNumberRegex);
     }
     
     // keywords
@@ -164,11 +165,11 @@ NSString *SMLSyntaxDefinitionGroupSpecialization = @"groupSpecialization";
     value = [syntaxDictionary objectForKey:SMLSyntaxDefinitionVariableRegex];
     if (value && ![value isEqual:@""]) {
         RETURN_NIL_IF_FALSE([value isKindOfClass:[NSString class]], @"NSString expected");
-        _variableRegex = value;
+        _variableRegex = [NSRegularExpression regularExpressionWithPattern:value options:NSRegularExpressionAnchorsMatchLines error:nil];
+        RETURN_NIL_IF_FALSE(_variableRegex, @"Incorrect regex syntax in %@", SMLSyntaxDefinitionVariableRegex);
         _beginVariableCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@""];
         _endVariableCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@""];
     } else {
-        _variableRegex = @"";
         // begin variable
         value = [syntaxDictionary valueForKey:SMLSyntaxDefinitionBeginVariable];
         if (value) {
@@ -209,7 +210,8 @@ NSString *SMLSyntaxDefinitionGroupSpecialization = @"groupSpecialization";
     value = [syntaxDictionary objectForKey:SMLSyntaxDefinitionSingleLineCommentRegex];
     if (value) {
         RETURN_NIL_IF_FALSE([value isKindOfClass:[NSString class]], @"NSString expected");
-        _singleLineCommentRegex = value;
+        _singleLineCommentRegex = [NSRegularExpression regularExpressionWithPattern:value options:NSRegularExpressionAnchorsMatchLines error:nil];
+        RETURN_NIL_IF_FALSE(_singleLineCommentRegex, @"Incorrect regex syntax in %@", SMLSyntaxDefinitionSingleLineCommentRegex);
     } else {
         _singleLineComments = [NSMutableArray arrayWithCapacity:2];
         

Fragaria/MGSClassicFragariaSyntaxDefinition.h
@@ -67,10 +67,10 @@
 
 /** A regular expression that matches a variable.
  *  @discussion This property supersedes beginVariableCharacterSet and
- *       endVariableCharacterSet. If this property is equal to @"",
+ *       endVariableCharacterSet. If this property is equal to nil,
  *       a syntax colourer should use these properties instead, otherwise they
  *       should be ignored. */
-@property (readonly) NSString *variableRegex;
+@property (readonly) NSRegularExpression *variableRegex;
 /**  Characters that may start a variable. */
 @property (readonly) NSCharacterSet *beginVariableCharacterSet;
 /**  Characters that may terminate a variable. */
@@ -80,7 +80,7 @@
  *  @discussion This property supersedes singleLineComments. If this property
  *      is nil, a syntax colourer should use singleLineComments instead,
  *      otherwise it is ignored. */
-@property (readonly) NSString *singleLineCommentRegex;
+@property (readonly) NSRegularExpression *singleLineCommentRegex;
 /** An array of strings that should mark the beginning of a single-line comment.
  *  This kind of comments extends from the characters that mark its start
  *  to the end of its line. */
@@ -100,7 +100,7 @@
  *      and decimalPointCharacter. If this property is nil, a syntax
  *      colourer should use those properties instead, otherwise they are
  *      ignored. */
-@property (readonly) NSString *numberDefinition;
+@property (readonly) NSRegularExpression *numberDefinition;
 /** Characters to be coloured as numbers, if numberDefinition is nil. This
  *  includes eventual decimal separators. */
 @property (readonly) NSCharacterSet *numberCharacterSet;

Fragaria/MGSClassicFragariaSyntaxParser.m
@@ -130,9 +130,8 @@ typedef NSInteger SMLSyntaxGroupInteger;
 }
 
 
-- (void)recognizeMatchesOfPattern:(NSString*)pattern ofGroup:(NSString*)group inString:(NSString *)documentString range:(NSRange)colouringRange atomicTokens:(BOOL)atomic
+- (void)recognizeMatchesOfPattern:(NSRegularExpression *)regex ofGroup:(NSString*)group inString:(NSString *)documentString range:(NSRange)colouringRange atomicTokens:(BOOL)atomic
 {
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:nil];
     if (!regex) return;
     
     [regex enumerateMatchesInString:documentString options:0 range:colouringRange usingBlock:^(NSTextCheckingResult *match, NSMatchingFlags flags, BOOL *stop) {
@@ -515,7 +514,7 @@ typedef NSInteger SMLSyntaxGroupInteger;
     NSString *rangeString = [rangeScanner string];
     NSUInteger rangeStringLength = [rangeString length];
     
-    if (![self.syntaxDefinition.variableRegex isEqual:@""]) {
+    if (self.syntaxDefinition.variableRegex) {
         [self recognizeMatchesOfPattern:self.syntaxDefinition.variableRegex ofGroup:MGSSyntaxGroupVariable inString:documentScanner.string range:rangeToRecolour atomicTokens:YES];
         return;
     }

commit b7f2ba724c61dba1d0651adfc31ea507da2be32e

Properly compute the list of used syntax groups (which broke in 3d0dfb9)

Fragaria/MGSClassicFragariaSyntaxDefinition.m
@@ -358,7 +358,7 @@ NSString *SMLSyntaxDefinitionGroupSpecialization = @"groupSpecialization";
         [res addObject:MGSSyntaxGroupKeyword];
     if ([self.autocompleteWords count] > 0)
         [res addObject:MGSSyntaxGroupAutoComplete];
-    if (![self.beginVariableCharacterSet mgs_isEmpty] || ![self.variableRegex isEqual:@""])
+    if (![self.beginVariableCharacterSet mgs_isEmpty] || self.variableRegex)
         [res addObject:MGSSyntaxGroupVariable];
     if (![self.secondString isEqual:@""] || ![self.firstString isEqual:@""])
         [res addObject:MGSSyntaxGroupString];
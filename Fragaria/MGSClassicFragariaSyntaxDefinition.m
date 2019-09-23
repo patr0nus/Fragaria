b7f2ba724c61dba1d0651adfc31ea507da2be32e
Daniele Cattaneo <daniele.cattaneo@alice.it>
2019-09-23 15:49:21 +0200

    Properly compute the list of used syntax groups (which broke in 3d0dfb9)

--- Fragaria/MGSClassicFragariaSyntaxDefinition.m
+++ Fragaria/MGSClassicFragariaSyntaxDefinition.m
@@ -358,7 +358,7 @@ NSString *SMLSyntaxDefinitionGroupSpecialization = @"groupSpecialization";
         [res addObject:MGSSyntaxGroupKeyword];
     if ([self.autocompleteWords count] > 0)
         [res addObject:MGSSyntaxGroupAutoComplete];
-    if (![self.beginVariableCharacterSet mgs_isEmpty] || ![self.variableRegex isEqual:@""])
+    if (![self.beginVariableCharacterSet mgs_isEmpty] || self.variableRegex)
         [res addObject:MGSSyntaxGroupVariable];
     if (![self.secondString isEqual:@""] || ![self.firstString isEqual:@""])
         [res addObject:MGSSyntaxGroupString];
